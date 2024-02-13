9851e5f68da0ba41d47df06b249f8c1495f6bbb2
victg <123119434+statistron@users.noreply.github.com>
2024-02-13 11:41:47 -0800

    Code and readme updates

--- marimo-cheat-sheet.py
+++ marimo-cheat-sheet.py
@@ -1,15 +1,19 @@
 import marimo
 
-__generated_with = "0.1.0"
+
+
+__generated_with = "0.2.4"
 app = marimo.App(width="full")
 
 
 @app.cell
 def __(mo):
     
-    mo.md("## **marimo** cheat sheet 0.2.4")
+    mo.md("## **marimo** cheat sheet  0.2.4")
     
     return
+
+
 
 
 
